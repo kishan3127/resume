8e178ddd8349a86bedfcc504d87017959bfdd9a3
Kishan Gopal Modi <16267345+kishan3127@users.noreply.github.com>
2024-11-25 16:56:32 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,55 +1,61 @@
-function getLanguageFromURL(url) {
-  const urlObj = new URL(url);
-  const pathSegments = urlObj.pathname.split('/');
-  return pathSegments[2]; 
-}
-
-function getMetadata(name) {
-  const attr = name && name.includes(':') ? 'property' : 'name';
-  const meta = [...document.head.querySelectorAll(`meta[${attr}="${name}"]`)].map((m) => m.content).join(', ');
-  return meta || '';
-}
-
-function generateSlug(name){
-  const slug = name
-        .toLowerCase()               
-        .trim()                      
-        .replace(/[^a-z0-9\s-]/g, '') 
-        .replace(/\s+/g, '-')        
-        .replace(/-+/g, '-');        
-  return slug
-}
 function generateArticleSchema() {
   const locationObj = window.location
   const pageLang = getLanguageFromURL(locationObj.href);
   const authorSlug = generateSlug(getMetadata('author') || '')
-  const authorPath = `${locationObj.origin}/blogs/${pageLang}/authors/${authorSlug}`
-  const schema = {
-    '@context': 'https://schema.org',
-    '@type': 'BlogPosting',
-    mainEntityOfPage: {
-      '@type': 'WebPage',
-      '@id': window.location.href,
-    },
-    headline: document.querySelector('title')?.innerText || 'No Title',
-    description: getMetadata('description') || '',
-    image: getMetadata('og:image') || '',
-    author: {
-      '@type': 'Person',
-      name: getMetadata('author') || '',
-      url: authorPath
-    },
-    publisher: {
-      '@type': 'Organization',
-      name: 'Keysight',
-      logo: {
-        '@type': 'ImageObject',
-        url: 'https://www.keysight.com/content/dam/keysight/en/img/gnav/keysight-logo.svg',
-      },
-    },
-    datePublished: getMetadata('publication-date') || '',
-    dateModified: getMetadata('last-modified') || ''
-  };
+  const authorPath = `${locationObj.origin}/blogs/${pageLang}/authors/${authorSlug}`;
+  const schema =  {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Kishan Gopal Modi",
+  "description": "I'm a FrontEnd Developer, based in Bikaner, Rajasthan. Working as a professional web developer from last 5+ years. Worked with international clients from various origins. Having expertise in trending JavaScript frameworks.
+",
+  "url": "https://kishan3127.github.io/resume",
+  "image": "https://kishan3127.github.io/resume/images/myProfile.jpeg",
+  "email": "kishangopalmodi@gmail.com",
+  "telephone": "+91-9057280689",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Near Minerwa Cinema, Chotina Well, K.E.M. Road",
+    "addressLocality": "Bikaner, Rajasthan",
+    "addressRegion": "RJ",
+    "postalCode": "334001",
+    "addressCountry": "In"
+  },
+  "jobTitle": "Lead Technical Consultant",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Perficient",
+    "url": "https://www.perficient.com/"
+  },
+  "alumniOf": {
+    "@type": "EducationalOrganization",
+    "name": "Rajasthan Technical University",
+    "url": "https://www.rtu.ac.in"
+  },
+  "skills": [
+    "JavaScript",
+    "React",
+    "Node.js",
+    "AEM UI"
+  ],
+  "hasOccupation": {
+    "@type": "Occupation",
+    "name": "Software Developer",
+    "estimatedSalary": {
+      "@type": "MonetaryAmount",
+      "currency": "INR",
+      "value": {
+        "@type": "QuantitativeValue",
+        "value": "2000000",
+        "unitText": "YEAR"
+      }
+    }
+  },
+  "sameAs": [
+    "https://www.linkedin.com/in/kishangopalmodi",
+    "https://github.com/kishan3127"
+  ]
+};
   const script = document.createElement('script');
   script.type = 'application/ld+json';
   script.textContent = JSON.stringify(schema);
